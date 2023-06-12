60e975f2f4ecdf634d2cf366f05ac20e86663b64
TheoLoeffelmann <theo_loeffelmann@hotmail.com>
2023-06-12 00:28:50 -0600

    Se generan clases DAO para consumo de sps y generación de dbcontext

---
+++ Rinku.DAO/CatRolesDb.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rinku.Entities;
+using Rinku.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rinku.DAO
+{
+    public class CatRolesDb
+    {
+        private readonly RinkuContext _db;
+
+        public CatRolesDb(RinkuContext db)
+        {
+            _db = db;
+        }
+
+
+        public async  Task<CatRoles> RolesCUD(int opc, CatRoles rol)
+        {
+            try
+            {
+                var response = _db.CatRoles.FromSqlInterpolated($"exec sp_CatRoles_CRUD @opc= {opc}, @Id = {rol.RoleId}, @Role={rol.RoleId}, @Deactivated = {rol.Deactivated}").AsAsyncEnumerable();
+                await foreach(var role in response)
+                {
+                    return role;
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("No se pudo llevar la operacion CUD de roles: " + ex.Message);
+            }
+        }
+
+        public async Task<IEnumerable<CatRoles>> GetRoles(int opc, CatRoles rol)
+        {
+            try
+            {
+                return await _db.CatRoles.FromSqlInterpolated($"exec sp_CatRoles_CRUD @opc= {opc}, @Id = {rol.RoleId}, @Role={rol.RoleId}, @Deactivated = {rol.Deactivated}").ToListAsync();
+                
+               
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<CatRoles> GetRole(int opc, CatRoles rol)
+        {
+            try
+            {
+                var response=  _db.CatRoles.FromSqlInterpolated($"exec sp_CatRoles_CRUD @opc= {opc}, @Id = {rol.RoleId}, @Role={rol.RoleId}, @Deactivated = {rol.Deactivated}").AsAsyncEnumerable();
+                await foreach (var item in response)
+                {
+                    return item;
+                }
+                return null;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("No se pudo encontrar el rol: "+ ex.Message);
+            }
+        }
+
+
+    }
+}
---
+++ Rinku.DAO/PaymentsDb.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rinku.Entities;
+using Rinku.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rinku.DAO
+{
+    public class PaymentsDb
+    {
+        private readonly RinkuContext _db;
+
+        public PaymentsDb(RinkuContext db)
+        {
+            this._db= db;
+        }
+
+        public async Task<Payments> PaymentsCUD(int opc, Payments payment)
+        {
+            try
+            {
+                var response = _db.Payments.FromSqlInterpolated($"exec sp_Payments_CRUD @opc={opc}, @Id={payment.PaymentId}, @TotalHours={payment.TotalHours}, @TotalPaymentDelivers= {payment.TotalPaymetDelivers}, @TotalPaymentBounus = {payment.TotalPaymentBounus}, @TotalWithholdings= {payment.TotaltWithholdings}, @TotalPaymentGroceryVouchers={payment.TotalPaymentsGroceryVouchers}, @TotalSalary={payment.TotalSalary}, @Deactivated= {payment.Deactivated}, @DeliveryId = {payment.DeliveryId}, @EmployeeId = {payment.EmployeeId} ").AsAsyncEnumerable();
+                await foreach( var item in response)
+                {
+                    return item;
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("No se pudo generar el proceso CUD para Pagos, error: " + ex.Message);
+            }
+        }
+
+        public async Task<IEnumerable<Payments>> GetPayments(int opc, Payments payment)
+        {
+            try
+            {
+                var response = _db.Payments.FromSqlInterpolated($"exec sp_Payments_CRUD @opc={opc}, @Id={payment.PaymentId}, @TotalHours={payment.TotalHours}, @TotalPaymentDelivers= {payment.TotalPaymetDelivers}, @TotalPaymentBounus = {payment.TotalPaymentBounus}, @TotalWithholdings= {payment.TotaltWithholdings}, @TotalPaymentGroceryVouchers={payment.TotalPaymentsGroceryVouchers}, @TotalSalary={payment.TotalSalary}, @Deactivated= {payment.Deactivated}, @DeliveryId = {payment.DeliveryId}, @EmployeeId = {payment.EmployeeId} ").AsAsyncEnumerable();
+                List<Payments> list= new List<Payments>();
+                await foreach (var item in response)
+                {
+                    Payments pay = new Payments();
+                     pay= item;
+                    list.Add(pay);
+                }
+                return list;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Ocurrio un error al intentar buscar los pagos " + ex.Message);
+            }
+        }
+
+        public async Task<Payments> GetPayment(int opc, Payments payment)
+        {
+            try
+            {
+                var response = _db.Payments.FromSqlInterpolated($"exec sp_Payments_CRUD @opc={opc}, @Id={payment.PaymentId}, @TotalHours={payment.TotalHours}, @TotalPaymentDelivers= {payment.TotalPaymetDelivers}, @TotalPaymentBounus = {payment.TotalPaymentBounus}, @TotalWithholdings= {payment.TotaltWithholdings}, @TotalPaymentGroceryVouchers={payment.TotalPaymentsGroceryVouchers}, @TotalSalary={payment.TotalSalary}, @Deactivated= {payment.Deactivated}, @DeliveryId = {payment.DeliveryId}, @EmployeeId = {payment.EmployeeId} ").AsAsyncEnumerable();
+                
+                await foreach (var item in response)
+                {
+                    return item;
+                    
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Ocurrio un error al intentar buscar los pagos " + ex.Message);
+            }
+        }
+
+
+    }
+}
